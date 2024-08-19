8ede54092464a5a192f37a2c052399b0c5ea07b9
David Malvoisin <davidmalvoisin@hotmail.fr>
2024-08-19 15:18:59 +0200

    init etl elections loire-atlantique

---
+++ etl_dossier_insee.py
@@ -0,0 +1,48 @@
+import pandas as pd
+pd.options.display.width = 200
+
+def traitement_insee(save=False):
+
+    #Expression régulière (REGEX) pour sélectionner toutes les communes de Loire Atlantique (code insee qui commence par 44)
+    regex_code_insee = "^44[0-9]{3}$"
+
+    #Regex pour sélectionner les colonnes pertinentes
+
+
+    input_path = "input/"
+    fichier = input_path + "dossier_complet.csv"
+    fichier_metadata = input_path + "meta_dossier_complet.csv"
+
+    df_insee = pd.read_csv(fichier, sep=";")
+    df_metadata = pd.read_csv(fichier_metadata, sep=";")
+
+    df_insee["CODGEO"] = df_insee["CODGEO"].apply(str)
+    print(df_insee.shape)
+    print(df_metadata.shape)
+
+    df_insee = df_insee[df_insee["CODGEO"].str.contains(regex_code_insee, regex=True)]
+
+    df_insee = df_insee.filter(regex=r"^(P(21|15|10)_(POP(H|F|0014|1529|3044|4559|6074|7589|90P)|CHOM1564)|C(21|15|10)_POP15P_CS7|CODGEO|SNHM21)$")
+    df_metadata = df_metadata[(df_metadata["COD_VAR"] != "CODGEO") & (df_metadata["COD_VAR"].isin(df_insee.columns))]
+
+    print(df_insee.head())
+    print(df_insee.shape)
+
+    print(df_metadata.head())
+    print(df_metadata.shape)
+    i = 0
+    for column in df_insee.columns:
+        i+=1
+        print(column, end=" ")
+        if i == 10:
+            i = 0
+            print()
+
+    output_path = "output/"
+    output = output_path + "dossier_complet.csv"
+    output_metadata = output_path + "meta_dossier_complet.csv"
+
+    if save:
+        print("Sauvegarde dossier complet df_insee traité")
+        df_insee.to_csv(output, sep=";", index=False)
+        df_metadata.to_csv(output_metadata, sep=";", index=False)
---
+++ main_etl.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from etl_dossier_insee import *
+
+
+
+def main_funtion():
+    print("Main ETL")
+    traitement_insee(True)
+
+
+if __name__ == "__main__":
+    main_funtion()
