bfd389518fc43bdc1f1314428d33403e73f2e414
Mickaël Seffar <mickael.seffar@gmail.com>
2024-08-20 16:07:38 +0200

    Fix path + noms variables

--- etl_dossier_asso_utilite_publique.py
+++ etl_dossier_asso_utilite_publique.py
@@ -1,41 +1,35 @@
 import pandas as pd
 
+pd.options.display.width = 200
+pd.options.display.max_columns = None
+pd.options.display.max_colwidth = None
+
 def traitement_asso_utilite_publique(save=False):
 
-    # Chemin vers le fichier CSV
-    df = pd.read_csv('/Users/mickael/Documents/Cours/EPSI/MSPR/Data/Data Associations/Liste associations reconnues d_utilité publique - avril 2021.csv', sep=';', encoding='latin1')
+    input_path = "input/"
+    fichier = input_path + "dossier_complet.csv"
 
-    # Configurations d'affichage pour Pandas
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.max_rows', None)
-    pd.set_option('display.max_colwidth', None)
-    pd.set_option('display.width', None)
+    df_asso_utilite_publique = pd.read_csv(fichier, sep=';', encoding='latin1')
 
     # Liste des colonnes à supprimer
     colonnes_inutiles = [
-        'OBJET', 'ADRESSE'
+        'id_ex', 'siret', 'rup_mi', 'gestion', 'groupement', 'objet_social',
+        'maj_time', 'siteweb', 'publiweb', 'dir_civilite', 'telephone', 'adrg',
+        'nature', 'objet','adrs_complement','adrs_numvoie','adrs_repetition',
+        'adrs_typevoie','adrs_libvoie','adrs_distrib', 'observation','position',
+        'titre_court'
     ]
 
     # Filtrer les colonnes inutiles
-    columns_to_drop = [col for col in df.columns if col in colonnes_inutiles or col.startswith('Unnamed')]
-    df_cleaned = df.drop(columns=columns_to_drop)
+    columns_to_drop = [col for col in df_asso_utilite_publique.columns if col in colonnes_inutiles or col.startswith('adrg')]
+    df_asso_utilite_publique = df_asso_utilite_publique.drop(columns=columns_to_drop)
 
-    # Afficher les 100 premières lignes du DataFrame nettoyé (optionnel)
-    # print(df_cleaned.head(100))
+    # Facultatif : Afficher les 100 premières lignes du DataFrame nettoyé
+    print(df_asso_utilite_publique.head(100))
 
-    # Filtrer les données où la colonne 'CP' commence par '44'
-    if 'CP' in df_cleaned.columns:
-        df_asso_utilite_publique = df_cleaned[df_cleaned['CP'].astype(str).str.startswith('44')]
-    else:
-        print("La colonne 'CP' n'existe pas dans le DataFrame.")
-
-    # Afficher les données filtrées
-    print(df_asso_utilite_publique)
-
-    # Chemin vers le fichier CSV de sortie (si vous souhaitez sauvegarder les résultats filtrés)
     output_path = "output/"
     output = output_path + "dossier_complet.csv"
 
     if save:
-        print("Sauvegarde dossier complet df_asso_utilite_publique traité")
+        print("Sauvegarde du fichier filtré")
         df_asso_utilite_publique.to_csv(output, sep=';', index=False)
--- etl_dossier_subvention_asso.py
+++ etl_dossier_subvention_asso.py
@@ -1,33 +1,32 @@
 import pandas as pd
 
+pd.options.display.width = 200
+pd.options.display.max_columns = None
+pd.options.display.max_colwidth = None
+
+
 def traitement_subvention_asso(save=False):
+    input_path = "input/"
+    fichier = input_path + "dossier_complet.csv"
 
-    # Chemin vers le fichier CSV
-    df = pd.read_csv('/Users/mickael/Documents/Cours/EPSI/MSPR/Data/Data Associations/Subventions aux associations versées par le Département de Loire-Atlantique.csv', sep=';', encoding='UTF-8')
+    df_subvention_asso = pd.read_csv(fichier, sep=';', encoding='UTF-8')
 
-    # Configurations d'affichage pour Pandas
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.max_rows', None)
-    pd.set_option('display.max_colwidth', None)
-    pd.set_option('display.width', None)
-
-    # Liste des colonnes à supprimer
     colonnes_inutiles = [
-        'objet', 'conditionsversement','dateconvention','nature','detailavantagesnature',
-        'estimationavantagesnature', 'referencedecision','notificationue','idrae'
+        'objet', 'conditionsversement', 'dateconvention', 'nature', 'detailavantagesnature',
+        'estimationavantagesnature', 'referencedecision', 'notificationue', 'idrae'
     ]
 
     # Filtrer les colonnes inutiles
-    columns_to_drop = [col for col in df.columns if col in colonnes_inutiles or col.startswith('montant')]
-    df_subvention_asso = df.drop(columns=columns_to_drop)
+    columns_to_drop = [col for col in df_subvention_asso.columns if
+                       col in colonnes_inutiles or col.startswith('montant')]
+    df_subvention_asso = df_subvention_asso.drop(columns=columns_to_drop)
 
     # Afficher les 100 premières lignes du DataFrame nettoyé (optionnel)
     print(df_subvention_asso.head(100))
-
 
     output_path = "output/"
     output = output_path + "dossier_complet.csv"
 
     if save:
-        print("Sauvegarde dossier complet df_insee traité")
+        print("Sauvegarde du fichier filtré")
         df_subvention_asso.to_csv(output, sep=';', index=False)
